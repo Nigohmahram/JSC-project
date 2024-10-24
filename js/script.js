aeab8b9cbaa3f9e3ef65f0218263ea08eb52c5a6
Khojiakbar <usmondev@outlook.kr>
2024-10-24 17:10:00 +0500

    Ma'lumot turlari

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert('Salom Dunyo');
+// alert('Salom Dunyo');
+
+// 2 xil o'zgaruvchilar
+
+// let age = 22;
+// console.log(age);
+// age = 23;
+// const name = 'Samar';
+// console.log(name);
+
+const name = 'Samar';
+let age = 22;
+var ages = 23;
+
+{
+	var agess = 24;
+}
